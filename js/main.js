fe12dabc5daf14fb1714ddc9e3a746ecf72fa7ab
oli79 <oabadch@gmail.com>
2021-05-10 13:29:53 +0200

    exercicis bootstrap nivell 1 - nivell 2

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $( "#create-acount" ).click(function() {
+        $("#modal-login").modal('toggle');
+      });
+
+      ScrollReveal().reveal('.carousel');
+      ScrollReveal().reveal('.section', { delay: 400});
+      ScrollReveal().reveal('.footer', {delay: 300});   
+});
