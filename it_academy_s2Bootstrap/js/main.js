098dab6d379b98240757e308007dda68ca3da69a
oli79 <oabadch@gmail.com>
2021-05-11 14:43:43 +0200

    nivell 3 exercici 3

--- it_academy_s2Bootstrap/js/main.js
+++ it_academy_s2Bootstrap/js/main.js
@@ -1,9 +1,5 @@
-$( document ).ready(function() {
-    $( "#create-acount" ).click(function() {
-        $("#modal-login").modal('toggle');
-      });
-
-      ScrollReveal().reveal('.carousel');
-      ScrollReveal().reveal('.section', { delay: 400});
-      ScrollReveal().reveal('.footer', {delay: 300});   
+$(document).ready(function () {
+  $("#create-acount").click(function () {
+    $("#modal-login").modal('toggle');
+  });
 });
