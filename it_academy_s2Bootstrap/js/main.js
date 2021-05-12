93f834e1a67a829fe58970cf677abaac08be498e
oli79 <oabadch@gmail.com>
2021-05-12 11:08:54 +0200

    animaciones en home page utilizando plugins

--- it_academy_s2Bootstrap/js/main.js
+++ it_academy_s2Bootstrap/js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $("#create-acount").click(function () {
     $("#modal-login").modal('toggle');
   });
+  // ScrollReveal().reveal('.carousel');
+  ScrollReveal().reveal('#title-section', { delay: 750});
+  ScrollReveal().reveal('#cards', {delay: 1000});
+  ScrollReveal().reveal('.footer', {delay: 750});
 });
